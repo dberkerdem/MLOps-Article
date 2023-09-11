196c680bc7392b62e9cfca9399d707a839ce6c44
Daglar Berk Erdem <dberkerdem@gmail.com>
2023-09-12 02:14:36 +0300

    created custom agent image

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # MLOps-Article
+
+
+  docker run -d --name clearml-agent-1 \
+  -e CLEARML_AGENT_K8S_CPU_LIMIT="1" \
+  -e CLEARML_AGENT_K8S_CPU_REQUEST="1" \
+  allegroai/clearml-agent:latest
---
+++ agent/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+LOWER_PIP_UPDATE_VERSION="$(echo "$PIP_UPDATE_VERSION" | tr '[:upper:]' '[:lower:]')"
+LOWER_CLEARML_AGENT_UPDATE_VERSION="$(echo "${CLEARML_AGENT_UPDATE_VERSION:-$TRAINS_AGENT_UPDATE_VERSION}" | tr '[:upper:]' '[:lower:]')"
+
+if [ "$LOWER_PIP_UPDATE_VERSION" = "yes" ] || [ "$LOWER_PIP_UPDATE_VERSION" = "true" ] ; then
+    python3 -m pip install -U pip
+elif [ ! -z "$LOWER_PIP_UPDATE_VERSION" ] ; then
+    python3 -m pip install pip$LOWER_PIP_UPDATE_VERSION ;
+fi
+
+echo "CLEARML_AGENT_UPDATE_VERSION = $LOWER_CLEARML_AGENT_UPDATE_VERSION"
+if [ "$LOWER_CLEARML_AGENT_UPDATE_VERSION" = "yes" ] || [ "$LOWER_CLEARML_AGENT_UPDATE_VERSION" = "true" ] ; then
+    python3 -m pip install clearml-agent -U
+elif [ ! -z "$LOWER_CLEARML_AGENT_UPDATE_VERSION" ] ; then
+    python3 -m pip install clearml-agent$LOWER_CLEARML_AGENT_UPDATE_VERSION ;
+fi
+
+python3 -m clearml_agent init
+python3 -m clearml_agent daemon --force-current-version --foreground
