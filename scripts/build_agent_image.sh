737053a5769e2c623c77663c6bcb7533c3d855ea
Daglar Berk Erdem <dberkerdem@gmail.com>
2023-09-18 12:55:08 +0300

    updated and refactored scripts

--- scripts/build_agent_image.sh
+++ scripts/build_agent_image.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-docker build -t clearml-agent-custom ./agent
+IMAGE_NAME="clearml-agent-custom"
+
+while [[ $# -gt 0 ]]
+do
+  key="$1"
+  case $key in
+    --image-name)
+      IMAGE_NAME="$2"
+      shift
+      shift
+      ;;
+    *)
+      shift
+      ;;
+  esac
+done
+
+docker build -t $IMAGE_NAME ./agent
--- scripts/run_workers.sh
+++ scripts/run_workers.sh
@@ -1,11 +1,40 @@
 #!/bin/bash
 
-NUM_WORKER=${1:-1}
+NUM_WORKER=1
+IMAGE_NAME="clearml-agent-custom"
 
-docker build -t clearml-agent-custom ./agent
+while [[ $# -gt 0 ]]
+do
+  key="$1"
+  case $key in
+    --num-workers)
+      NUM_WORKER="$2"
+      shift
+      shift
+      ;;
+    --image-name)
+      IMAGE_NAME="$2"
+      shift
+      shift
+      ;;
+    *)
+      shift
+      ;;
+  esac
+done
+
+find_next_worker_number() {
+  local n=1
+  while docker container inspect clearml-agent-$n > /dev/null 2>&1; do
+    let "n++"
+  done
+  echo $n
+}
 
 for (( i=1; i<=$NUM_WORKER; i++ ))
 do
-  echo "Running iteration $i"
-  docker run -it -d --rm --name clearml-agent-$i clearml-agent-custom
+  next_worker_number=$(find_next_worker_number)
+  worker_name="clearml-agent-$next_worker_number"
+  echo "Running container with name $worker_name"
+  docker run -it -d --rm --name $worker_name --env-file agent/.env -e CLEARML_WORKER_NAME=$worker_name $IMAGE_NAME
 done
