484f72fe011620333ef6a1f5153f2170ed47ce99
Daglar Berk Erdem <dberkerdem@gmail.com>
2023-09-21 10:04:09 +0300

    fixed user data

--- agent/tf/main.tf
+++ agent/tf/main.tf
@@ -15,23 +15,33 @@ resource "aws_instance" "my_instance" {
   vpc_security_group_ids = [var.security_group_id]
 
   user_data = <<-EOF
-              #!/bin/bash
-              
-              sudo yum update -y
-              sudo yum install docker -y
-              sudo service docker start
-              sudo usermod -a -G docker ec2-user
-              sudo chkconfig docker on
-              sudo yum install -y git
-              
-              git clone var.repo_url
-              cd MLOps-Article
-              echo '${local.env_content}' > ./agent/.env
+    #!/bin/bash
 
-              sudo chmod +x scipts/*
-              sh scripts/build_agent_image.sh
-              sh scripts/run_workers.sh --num-workers 1
-              EOF
+    # Log file for debugging
+    exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
+
+    sudo yum update -y
+    sudo yum install docker -y
+    sudo service docker start
+    sudo usermod -a -G docker ec2-user
+    sudo chkconfig docker on
+    sudo yum install -y git
+
+    # Clone the repo
+    git clone ${var.repo_url}
+    if [ $? -ne 0 ]; then
+        echo "Failed to clone the repo"
+        exit 1
+    fi
+
+    cd MLOps-Article
+    echo '${local.env_content}' > ./agent/.env
+
+    sudo chmod +x scripts/*
+    ./scripts/build_agent_image.sh
+    ./scripts/run_workers.sh --num-workers 1
+  EOF
+
 
   tags = {
     Name = var.instance_name
